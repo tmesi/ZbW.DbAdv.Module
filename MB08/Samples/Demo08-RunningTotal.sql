c65aa2d7becc136824ee53f33a841b9c658bcfbc
Mesaros, Thomas <thomas.mesaros@unisg.ch>
2026-01-26 15:36:05 +0100

    adding additional sample

--- MB08/Samples/Demo08-RunningTotal.sql
+++ MB08/Samples/Demo08-RunningTotal.sql
@@ -4,7 +4,20 @@ SELECT		sod.SalesOrderId,
 			sod.SalesOrderDetailID,
 			sod.LineTotal,
 			RunningTotal		 = SUM(LineTotal) OVER (ORDER BY SalesOrderDetailID 
-														ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) 
+														/*ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW*/) 
 FROM		Sales.SalesOrderDetail	AS sod
 WHERE		sod.SalesOrderId		= 43659
 ORDER BY	sod.SalesOrderDetailID;
+
+/*
+SELECT		soh.OrderDate,
+			sod.SalesOrderId,
+			sod.LineTotal,
+			ROW_NUMBER() OVER (ORDER BY soh.OrderDate),
+			RANK() OVER (ORDER BY soh.OrderDate)
+FROM		Sales.SalesOrderDetail	AS sod
+			INNER JOIN
+			Sales.SalesOrderHeader AS soh
+				ON soh.SalesOrderID = sod.SalesOrderID
+ORDER BY	sod.SalesOrderDetailID;
+*/
